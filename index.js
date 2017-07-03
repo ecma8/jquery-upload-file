d35cd5cb6921e2310c62ceacef936651a48f73f1
ecma8 <756932563@qq.com>
2017-07-03 14:57:43 +0800

    upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');                                                                                                                                                                                                                                           
+var router = express.Router();
+var app=express()
+var multiparty = require('multiparty');
+var util = require('util');
+var fs = require('fs');
+
+/* 上传页面 */
+app.get('/index', function (req, res) {
+    res.sendFile( __dirname + "/index.html" );
+})
+/* 上传*/
+app.post('/uploading', function(req, res, next){
+  //生成multiparty对象，并配置上传目标路径
+  var form = new multiparty.Form({uploadDir: './public_img/'});
+  //上传完成后处理
+  form.parse(req, function(err, fields, files) {
+    var filesTmp = JSON.stringify(files,null,2);
+    if(err){
+      console.log('parse error: ' + err);
+    } else {
+      // console.log('parse files: ' + filesTmp);
+      var inputFile = files.file[0];
+      console.log(files);
+      var uploadedPath = inputFile.path;
+      // var dstPath = './public_img/' + inputFile.originalFilename;
+      // fs.rename(uploadedPath, dstPath, function(err) {
+      //   if(err){
+      //     console.log('rename error: ' + err);
+      //   } else {
+      //     console.log('rename ok');
+      //   }
+      // });
+    }
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.end(filesTmp);
+ });
+});
+app.use('/public',express.static(__dirname+'/public'));
+app.use('/public_img',express.static(__dirname+'/public_img'));
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
+module.exports = router;
---
+++ public/jquery-upload-file.js
@@ -0,0 +1,32 @@
+(function($){
+	$.fn.extend({
+		uploadFile:function(obj){
+			return this.each(function(){
+				var type=obj.type?obj.type:'POST';
+				var url=obj.url?obj.url:'';//必填;
+				var dataType=obj.dataType?obj.dataType:'json';//必填;
+				var returnSuccess=obj.returnSuccess;
+				var returnError=obj.returnError;
+				$(this).on('change',function(){
+					var formdata=new FormData();
+					formdata.append("file",$(this)[0].files[0]);
+					$.ajax({
+						type : type,
+						url : url,
+						data : formdata,
+						cache : false,
+						processData : false,
+						contentType : false,
+						dataType:dataType,
+						success : function(data){
+							returnSuccess(data);
+						},
+						error : function(data){
+							returnError(data);
+						}
+					})
+				})
+			})
+		}
+	})
+})(jQuery)
